86b74c861efa916d8f65e6ce2f6083214778f2f7
qcwxezda <hy_pi_corism@live.com>
2025-05-27 12:30:10 -0400

    update batch to delete existing .zip before packing

--- zip.bat
+++ zip.bat
@@ -1,5 +1,6 @@
 @echo off
-for %%f in ("%CD%") do set "copy_dir=%%~nxf"
-robocopy . "%copy_dir%" /E /JOB:zip /xd "%copy_dir%"
+for %%f in ("%cd%") do set "copy_dir=%%~nxf"
+del "%copy_dir%.zip" /s /q
+robocopy . "%copy_dir%" /e /job:zip /xd "%copy_dir%"
 "C:\Program Files\7-Zip\7z.exe" a -tzip "%copy_dir%" "%copy_dir%\*" -r
-rmdir "%copy_dir%" /S /Q
+rmdir "%copy_dir%" /s /q
